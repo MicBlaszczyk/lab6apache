ef960c41be213c7ff2ce3b695175f8c1701bc4a6
MicBlaszczyk <michal.blaszczyk@pollub.edu.pl>
2018-12-04 20:48:09 +0100

    Update main.java

--- main.java
+++ main.java
@@ -11,21 +11,53 @@ class main {
 
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/mydb", "root", null
+                    "jdbc:mysql://localhost:3306/mydb", "mbl", null
             );
 
-            //String ct = "CREATE TABLE IF NOT EXISTS users (username VARCHAR(30), password VARCHAR(30), name VARCHAR(30));";
+            String ct = "CREATE TABLE IF NOT EXISTS users (username VARCHAR(30), password VARCHAR(30), name VARCHAR(30));";
 
-            //statement = conn.createStatement();
+            statement = conn.createStatement();
 
-            //statement.executeUpdate(ct);
+            statement.executeUpdate(ct);
 
             Scanner in = new Scanner(System.in);
-            while(true){
-  System.out.println("'a' to add, 'p' to print all rows");
+            while(true) {
+                System.out.println("'a' to add, 'p' to print all rows");
                 String choice = in.nextLine();
-}
+                if ("p".equals(choice)) {
+                    statement = conn.createStatement();
+                    String query = "select * from users;";
+                    ResultSet rs = statement.executeQuery(query);
+                    System.out.println("Users:");
+                    while (rs.next()) {
+                        String username = rs.getString("username");
+                        String password = rs.getString("password");
+                        String name = rs.getString("name");
+
+                        System.out.print(username);
+                        System.out.print(", ");
+                        System.out.print(password);
+                        System.out.print(", ");
+                        System.out.println(name);
+                    }
+                }
+                if ("a".equals(choice)) {
+                    System.out.print("Username: ");
+                    String username = in.nextLine();
+                    System.out.print("Password: ");
+                    String password = in.nextLine();
+                    System.out.print("Name: ");
+                    String name = in.nextLine();
+
+                    statement = conn.createStatement();
+
+                    String query = "INSERT INTO users VALUES('"+username+"', '"+password+"', '"+name+"');";
+
+                    statement.executeUpdate(query);
+                }
+            }
         } catch(Exception e) {
+            System.out.print("-------------EXEPTION-------------");
             System.out.println(e);
         }
     }
