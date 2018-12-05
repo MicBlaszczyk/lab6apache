90dd7b8f86d6117647d4e7563da154f91fa6ce3e
MicBlaszczyk <michal.blaszczyk@pollub.edu.pl>
2018-12-05 22:25:58 +0100

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class main {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306";
+    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/mydb";
 
     //  Database credentials
     static final String USER = "mbl";
